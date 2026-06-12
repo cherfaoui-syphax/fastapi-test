878eb6f510821d40a6d4bf2558e4da71129979ab
cherfaoui-syphax <syphax2000.cherfaoui@gmail.com>
2026-06-12 12:20:15 +0200

    set debug level to info

--- src/cli/cli.py
+++ src/cli/cli.py
@@ -11,7 +11,7 @@ load_dotenv()
 
 # Configure logging
 logging.basicConfig(
-    level=logging.WARNING,
+    level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 logger = logging.getLogger(__name__)
